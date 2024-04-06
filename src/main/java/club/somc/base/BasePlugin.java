1a3454dddbffe6879f7c99d63e8805c7f302a3f3
Dylan Garvis <dylan@garvis.dev>
2024-04-06 09:53:27 -0400

    Added nats, protobuf, and config.

--- src/main/java/club/somc/base/BasePlugin.java
+++ src/main/java/club/somc/base/BasePlugin.java
@@ -3,14 +3,31 @@ package club.somc.base;
 import club.somc.base.commands.ReturnToBaseCommand;
 import club.somc.base.commands.SetBaseCommand;
 import club.somc.base.listeners.BaseListener;
+import io.nats.client.Connection;
+import io.nats.client.Nats;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.IOException;
 
 
 public class BasePlugin extends JavaPlugin {
 
+    Connection nc;
+
     @Override
     public void onEnable() {
+        super.onEnable();
+        this.saveDefaultConfig();
+
         Datastore ds = new Datastore(this);
+
+        try {
+            this.nc = Nats.connect(getConfig().getString("natsUrl"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
 
         this.getCommand("setbase").setExecutor(new SetBaseCommand(ds));
         this.getCommand("base").setExecutor(new ReturnToBaseCommand(ds));
